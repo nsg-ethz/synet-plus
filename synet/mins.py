d345f6b04b06607e67b80b4f4028e4025628a83b
Ahmed El-Hassany <eahmed@ethz.ch>
2017-06-26 19:15:31 +0200

    Add max with select

--- synet/mins.py
+++ synet/mins.py
@@ -21,6 +21,41 @@ def get_max(*args):
         return max2(args[0], get_max(*args[1:]))
 
 
+def max2_eval(eval_func, t1, t2):
+    """Returns the t1 or t2 based if eval_func(t1) >= eval_func(t2)"""
+    return z3.If(eval_func(t1) >= eval_func(t2), t1, t2)
+
+
+def max2_eval_select(select_func, select_val, eval_func, none_value, t1, t2):
+    """
+    This is a bit more complicated max function with lots of nobs!
+    if select_func(t1) == select_val && select_func(t1) == select_val, then act a max2_eval
+    if one of t1 or t2 select_val(tx) == select_val then return it
+    if none of t1 and t2 matches the input then return the none_value
+    """
+    return z3.If(
+        z3.And(select_func(t1) == select_val, select_func(t2) == select_val),
+        max2_eval(eval_func, t1, t2),
+        z3.If(
+            select_func(t1) == select_val,
+            t1,
+            z3.If(select_func(t2) == select_val,
+                  t2,
+                  none_value
+                  )
+        ))
+
+
+def get_max_eval_select(select_func, select_val, eval_func, none_value, *args):
+    """See max2_eval_select"""
+    assert len(args) >= 2
+    if len(args) == 2:
+        return max2_eval_select(select_func, select_val, eval_func, none_value, *args)
+    else:
+        return max2_eval_select(select_func, select_val, eval_func, none_value, args[0],
+                                get_max_eval_select(select_func, select_val, eval_func ,none_value, *args[1:]))
+
+
 def min2(t1, t2):
     """
     Get the minimum of list of two numerical t1 and t2
@@ -50,7 +85,7 @@ def min2_eval_select(select_func, select_val, eval_func, none_value, t1, t2):
     This is a bit more complicated min function with lots of nobs!
     if select_func(t1) == select_val && select_func(t1) == select_val, then act a min2_eval
     if one of t1 or t2 select_val(tx) == select_val then return it
-    if none of t1 and t2 mathes the input then return the none_value
+    if none of t1 and t2 matches the input then return the none_value
     """
     return z3.If(
         z3.And(select_func(t1) == select_val, select_func(t2) == select_val),
