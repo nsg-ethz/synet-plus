bd3ba885046e73210dfe62fb2a07b0b3d58abe9a
Ahmed El-Hassany <eahmed@ethz.ch>
2017-09-18 23:09:33 +0200

    add scripts to help with generating OSPF inputs

---
+++ eval_scripts/ospf_generate_reqs.py
@@ -0,0 +1,370 @@
+import argparse
+import logging
+import random
+import sys
+import os
+
+import numpy
+
+import networkx as nx
+
+from synet.utils.common import PathReq
+from synet.utils.common import ECMPPathsReq
+from synet.utils.common import PathOrderReq
+from synet.utils.common import Protocols
+from synet.utils.common import KConnectedPathsReq
+from synet.utils.topo_gen import read_topology_zoo_netgraph
+from synet.utils.smt_context import VALUENOTSET
+from synet.synthesis.ospf_heuristic import OSPFSyn
+from synet.synthesis.connected import ConnectedSyn
+
+from networkx.drawing.nx_agraph import write_dot
+
+
+def set_levels(graph, node, level):
+    """
+    Number levels in the tree
+    g.node[node]['level'] = level
+    This function is called recurivesly on the children with level +1
+    Return the max level reached
+    """
+    graph.node[node]['level'] = level
+    rets = []
+    for _, neighbor in graph.out_edges(node):
+        rets.append(set_levels(graph, neighbor, level + 1))
+    if graph.out_degree(node) == 0:
+        rets = [level]
+    return max(rets)
+
+
+def get_different_paths(g, source, min_disjoint, rand):
+    # First get a BFS Tree
+    stree = nx.bfs_tree(g, source)
+    max_level = set_levels(stree, source, 0)
+
+    # Then connect all nodes in the BFS Tree to all nodes
+    # the on a level lower than itself (if the edge exists on the original graph)
+    for slevel in range(0, max_level):
+        snodes = [node for
+                  node in stree.nodes()
+                  if stree.node[node]['level'] == slevel]
+        for dlevel in range(slevel + 1, max_level + 1):
+            dnodes = [node for
+                      node in
+                      stree.nodes()
+                      if stree.node[node]['level'] == dlevel]
+            for src in snodes:
+                for dst in dnodes:
+                    if g.has_edge(src, dst):
+                        stree.add_edge(src, dst)
+
+    # Connect leave nodes to each other if they don't create cycles
+    leaves = [node for node in stree.nodes() if stree.out_degree(node) == 0]
+    while len(leaves) > 0:
+        node = rand.choice(leaves)
+        for neighbor, _ in g.in_edges(node):
+            if not stree.has_edge(node, neighbor):
+                stree.add_edge(neighbor, node)
+        # New leaves
+        new_leaves = [node for node in stree.nodes() if stree.out_degree(node) == 1]
+        if set(new_leaves) == set(leaves):
+            # We hit a stable point and we cannot connect new leaves
+            # Without creating cycles
+            break
+        else:
+            leaves = new_leaves
+
+    all_paths = []
+    for node in g.nodes():
+        if node == source:
+            continue
+        paths = list(nx.all_simple_paths(stree, source, node))
+        unique = []
+        for path in paths:
+            if path not in unique:
+                unique.append(path)
+        if len(paths) >= min_disjoint:
+            all_paths.append(paths)
+    return all_paths
+
+
+def get_reqs(graph, reqsize, k, rand):
+    paths = []
+    while len(paths) < reqsize:
+        source = rand.choice(graph.nodes())
+        paths = get_different_paths(graph, source, k, rand)
+    selected = rand.sample(paths, reqsize)
+    reqs = {}
+    for lpaths in selected:
+        edges = []
+        for path in lpaths:
+            for pair in zip(path[0::1], path[1::1]):
+                edges.append(pair)
+        pmap = {}
+        for pair in edges:
+            if pair in pmap:
+                continue
+            pmap[pair] = float(edges.count(pair)) / len(edges)
+        path_prop = []
+        for path in lpaths:
+            curr_prop = 0.0
+            for pair in zip(path[0::1], path[1::1]):
+                curr_prop += pmap[pair]
+            path_prop.append(curr_prop / len(lpaths))
+        diff = (1.0 - sum(path_prop)) / len(path_prop)
+        path_prop = [p + diff for p in path_prop]
+        sindex = list(numpy.random.choice([i for i in range(len(lpaths))], size=k, replace=False, p=path_prop))
+        reqs[(lpaths[0][0], lpaths[0][-1])] = [lpaths[i] for i in sindex]
+    return reqs
+
+
+def generate_simple_reqs(topo, reqsize, rand):
+    """Generate reqsize of PathReq"""
+    paths = get_reqs(graph=topo, reqsize=reqsize, k=1, rand=rand)
+    reqs = []
+    for plist in paths.values():
+        assert len(plist) >= 1
+        path = plist[0]
+        req = PathReq(Protocols.OSPF, path[-1], path, False)
+        reqs.append(req)
+    return reqs
+
+
+def generate_ecmp_reqs(topo, reqsize, ecmp, rand):
+    """Generate reqsize of ECMPPathsReq each has ecmp paths"""
+    paths = get_reqs(graph=topo, reqsize=reqsize, k=ecmp, rand=rand)
+    reqs = []
+    for plist in paths.values():
+        assert len(plist) >= ecmp
+        req = ECMPPathsReq(Protocols.OSPF, plist[0][-1],
+                         [PathReq(Protocols.OSPF, path[-1], path, False)
+                          for path in plist[:ecmp]], False)
+        reqs.append(req)
+    return reqs
+
+
+
+def generate_ordered_reqs(topo, reqsize, ordered, rand):
+    """Generate reqsize of PathOrderReq each has ordered paths"""
+    paths = get_reqs(graph=topo, reqsize=reqsize, k=ordered, rand=rand)
+    reqs = []
+    for plist in paths.values():
+        assert len(plist) == ordered
+        req = PathOrderReq(Protocols.OSPF, plist[0][-1],
+                         [PathReq(Protocols.OSPF, path[-1], path, False)
+                          for path in plist], False)
+        reqs.append(req)
+    return reqs
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generate OSPF Requiremetns for a given topology.')
+    parser.add_argument('-f', type=str, default=None,
+                        help='read topology zoo graphml file')
+    parser.add_argument('--seed', type=int, default=0,
+                        help='The seed of the random generator')
+
+    args = parser.parse_args()
+    topology_file = args.f
+    seed = args.seed
+    assert topology_file
+
+    if not seed:
+        seed = random.randint(0, sys.maxint)
+        print "Generated new seed", seed
+
+    print "Using SEED:", seed
+
+    out_file = """
+from synet.utils.common import PathReq
+from synet.utils.common import ECMPPathsReq
+from synet.utils.common import PathOrderReq
+from synet.utils.common import Protocols
+from synet.utils.common import KConnectedPathsReq\n
+"""
+    out_file += "topology_file = '%s'\n" % topology_file
+    out_file += "seed = %d\n" % seed
+
+    rand = random.Random(seed)
+
+    topo = read_topology_zoo_netgraph(topology_file)
+
+    # Synthesize Connected
+    conn_syn = ConnectedSyn([], topo, full=True)
+    conn_syn.synthesize()
+    # All routers are OSPF Enabled
+    for node in topo.local_routers_iter():
+        topo.enable_ospf(node, 100)
+
+    for reqsize in [1, 2, 4, 8, 16][:1]:
+        simple_reqs = []
+        simple_vals = []
+        ecmp_reqs = []
+        ecmp_vals = []
+        ecmp_generated_reqs = []
+        k_reqs = []
+        k_vals = []
+        order_reqs = []
+        order_vals = []
+        generated = False
+        while not generated:
+            # All OSPF Costs are initially empty
+            for src, dst in topo.edges_iter():
+                if not topo.is_local_router_edge(src, dst):
+                    continue
+                topo.set_edge_ospf_cost(src, dst, VALUENOTSET)
+            print "X" * 40
+            print "Generating Simple for reqsize=%d" % (reqsize,)
+            reqs = generate_simple_reqs(topo, reqsize, rand)
+            print "Done Generating Simple"
+            ospf = OSPFSyn(topo, gen_paths=100)
+            for req in reqs:
+                ospf.add_req(req)
+            ospf.synthesize()
+            if ospf.removed_reqs:
+                print "Generating Reqs are unsatifiable for reqsize", reqsize, "Trying again"
+                continue
+            generated = True
+            ospf.update_network_graph()
+            req_name = "reqs_simple_%d" % reqsize
+            simple_reqs.append(req_name)
+            out_file += "%s = [\n" % (req_name)
+            for req in reqs:
+                out_file += "    %s,\n" % repr(req)
+            out_file += "]\n\n"
+            vals_name = "edges_cost_simple_%d" % reqsize
+            simple_vals.append(vals_name)
+            out_file += "%s = [\n" % vals_name
+            for src, dst in topo.edges_iter():
+                out_file += '    ("%s", "%s", %d),\n' % (src, dst, topo.get_edge_ospf_cost(src, dst))
+            out_file += "]\n\n"
+        out_file += "reqs_simple = [%s]\n\n" % ",".join(simple_reqs)
+        out_file += "reqs_simple_vals = [%s]\n\n" % ",".join(simple_vals)
+        out_file += "#" * 20
+        out_file += "\n\n"
+        for ecmp in [2]:
+            generated = False
+            while not generated:
+                # All OSPF Costs are intially empty
+                for src, dst in topo.edges_iter():
+                    if not topo.is_local_router_edge(src, dst):
+                        continue
+                    topo.set_edge_ospf_cost(src, dst, VALUENOTSET)
+                print "X" * 40
+                print "Generating ECMP for reqsize=%d, ecmp=%d" % (reqsize, ecmp)
+                reqs = generate_ecmp_reqs(topo, reqsize, ecmp, rand)
+                print "Done Generating ECMP"
+                ospf = OSPFSyn(topo, gen_paths=100)
+                for req in reqs:
+                    ospf.add_req(req)
+                ospf.synthesize()
+                if ospf.removed_reqs:
+                    print "Generating Reqs are unsatifiable for reqsize", reqsize, "Trying again"
+                    continue
+                generated = True
+                ecmp_generated_reqs.append(reqs)
+                ospf.update_network_graph()
+                req_name = "reqs_ecmp_%d_%d" % (reqsize, ecmp)
+                ecmp_reqs.append(req_name)
+                out_file += "%s = [\n" % (req_name)
+                for req in reqs:
+                    out_file += "    %s,\n" % repr(req)
+                out_file += "]\n\n"
+                vals_name = "edges_cost_ecmp_%d_%d" % (reqsize, ecmp)
+                ecmp_vals.append(vals_name)
+                out_file += "%s = [\n" % vals_name
+                for src, dst in topo.edges_iter():
+                    out_file += '    ("%s", "%s", %d),\n' %(src, dst, topo.get_edge_ospf_cost(src, dst))
+                out_file += "]\n\n"
+        out_file += "reqs_ecmp = [%s]\n\n" % ",".join(ecmp_reqs)
+        out_file += "reqs_ecmp_vals = [%s]\n\n" % ",".join(ecmp_vals)
+        out_file += "#" * 20
+        out_file += "\n\n"
+        for ecmp_reqs in ecmp_generated_reqs:
+            print "X" * 40
+            print "Generating Kconneced for reqsize=%d, k=%d" % (reqsize, len(ecmp_reqs))
+            for src, dst in topo.edges_iter():
+                if not topo.is_local_router_edge(src, dst):
+                    continue
+                topo.set_edge_ospf_cost(src, dst, VALUENOTSET)
+
+            kreqs = []
+            for req in ecmp_reqs:
+                kreqs.append(KConnectedPathsReq(req.protocol, req.dst_net, req.paths, False))
+
+            ospf2 = OSPFSyn(topo, gen_paths=100)
+            for req in kreqs:
+                ospf2.add_req(req)
+                ospf2.synthesize()
+            assert not ospf2.removed_reqs
+            ospf2.update_network_graph()
+            req_name = "reqs_kconnected_%d_%d" % (reqsize, ecmp)
+            k_reqs.append(req_name)
+            out_file += "%s = [\n" % (req_name)
+            for req in kreqs:
+                out_file += "    %s,\n" % repr(req)
+            out_file += "]\n\n"
+            vals_name = "edges_cost_kconnected_%d_%d" % (reqsize, ecmp)
+            k_vals.append(vals_name)
+            out_file += "%s = [\n" % vals_name
+            for src, dst in topo.edges_iter():
+                out_file += '    ("%s", "%s", %d),\n' % (src, dst, topo.get_edge_ospf_cost(src, dst))
+            out_file += "]\n\n"
+
+        out_file += "reqs_kconnected = [%s]\n\n" % ",".join(k_reqs)
+        out_file += "reqs_kconnected_vals = [%s]\n\n" % ",".join(k_vals)
+        out_file += "#" * 20
+        out_file += "\n\n"
+        continue
+        for pathorder in [2]:
+            generated = False
+            while not generated:
+                # All OSPF Costs are intially empty
+                for src, dst in topo.edges_iter():
+                    if not topo.is_local_router_edge(src, dst):
+                        continue
+                    topo.set_edge_ospf_cost(src, dst, VALUENOTSET)
+                print "X" * 40
+                print "Generating PathOrder for reqsize=%d, ordered paths=%d" % (reqsize, pathorder)
+                reqs = generate_ordered_reqs(topo, reqsize, pathorder, rand)
+                print "Done Generating PathOrdered"
+
+                ospf = OSPFSyn(topo, gen_paths=100)
+                for req in reqs:
+                    ospf.add_req(req)
+
+                ospf.synthesize()
+                if ospf.removed_reqs:
+                    print "Generating Reqs are unsatifiable for reqsize", reqsize, "Trying again"
+                    continue
+                generated = True
+                ospf.update_network_graph()
+                req_name = "reqs_order_%d_%d" % (reqsize, pathorder)
+                order_reqs.append(req_name)
+                out_file += "%s = [\n" % (req_name)
+                for req in reqs:
+                    out_file += "    %s,\n" % repr(req)
+                out_file += "]\n\n"
+                vals_name = "edges_cost_order_%d_%d" % (reqsize, pathorder)
+                order_vals.append(vals_name)
+                out_file += "%s = [\n" % vals_name
+                for src, dst in topo.edges_iter():
+                    out_file += '    ("%s", "%s", %d),\n' %(src, dst, topo.get_edge_ospf_cost(src, dst))
+                out_file += "]\n\n"
+        out_file += "reqs_order = [%s]\n\n" % ",".join(order_reqs)
+        out_file += "reqs_order_vals = [%s]\n\n" % ",".join(order_vals)
+        out_file += "#" * 20
+        out_file += "\n\n"
+
+    dirname = os.path.dirname(topology_file)
+    topo_name = os.path.basename(topology_file).split('.graphml')[0]
+    out_name = "%s_ospf_reqs.py" % topo_name
+    out_path = os.path.join(dirname, out_name)
+    with open(out_path, 'w') as file:
+        print "Writing to", out_path
+        file.write(out_file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ eval_scripts/topo_selector.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""
+Select which topologies to be used in evaluations
+"""
+
+
+import glob
+import os
+from os.path import basename
+from shutil import copyfile
+from shutil import rmtree
+
+from synet.utils.topo_gen import read_topology_zoo_netgraph
+
+
+__author__ = "Ahmed El-Hassany"
+__email__ = "a.hassany@gmail.com"
+
+
+SMALL_DIR = 'topos/small'
+MID_DIR = 'topos/mid'
+LARGE_DIR = 'topos/large'
+
+
+def select_topologies():
+    topos = {}
+    for file in glob.glob("topos/topozoo/*.graphml"):
+        topo = read_topology_zoo_netgraph(file)
+        if len(topo.nodes()) < 32 or len(topo.nodes()) > 200:
+            continue
+        topos[file] = topo
+
+    def cmp(x, y):
+        if len(topos[x].nodes()) < len(topos[y].nodes()):
+            return -1
+        elif len(topos[x].nodes()) > len(topos[y].nodes()):
+            return 1
+        elif len(topos[x].edges()) < len(topos[y].edges()):
+            return -1
+        elif len(topos[x].edges()) > len(topos[y].edges()):
+            return 1
+        return 0
+
+    ordered = sorted(topos.keys(), cmp=cmp, reverse=True)
+    small = ordered[-5:]
+    mid = ordered[(1 * (len(ordered) / 5)) - 3:(1 * (len(ordered) / 5)) + 2]
+    large = ordered[:5]
+
+    small = dict([(name, topos[name]) for name in small])
+    mid = dict([(name, topos[name]) for name in mid])
+    large = dict([(name, topos[name]) for name in large])
+    return small, mid, large
+
+
+def copy_topolgies(small, mid, large):
+    print "Cleaning folders"
+    if os.path.exists(SMALL_DIR):
+        rmtree(SMALL_DIR)
+    if os.path.exists(MID_DIR):
+        rmtree(MID_DIR)
+    if os.path.exists(LARGE_DIR):
+        rmtree(LARGE_DIR)
+    print "Creating new dirs"
+    os.mkdir(SMALL_DIR)
+    os.mkdir(MID_DIR)
+    os.mkdir(LARGE_DIR)
+
+    print "Small:"
+    for topo in small:
+        name = basename(topo)
+        print "\tCopying", name, "#nodes", len(small[topo].nodes()), \
+            "#edges", len(small[topo].edges())
+        copyfile(topo, "%s" % os.path.join(SMALL_DIR, name))
+
+    print "Mid:"
+    for topo in mid:
+        name = basename(topo)
+        print "\tCopying", name, "#nodes", len(mid[topo].nodes()), \
+            "#edges", len(mid[topo].edges())
+        copyfile(topo, "%s" % os.path.join(MID_DIR, name))
+
+    print "Large:"
+    for topo in large:
+        name = basename(topo)
+        print "\tCopying", name, "#nodes", len(large[topo].nodes()), \
+            "#edges", len(large[topo].edges())
+        copyfile(topo, "%s" % os.path.join(LARGE_DIR, name))
+
+
+def main():
+    small, mid, large = select_topologies()
+    copy_topolgies(small, mid, large)
+
+
+if __name__ == '__main__':
+    main()
