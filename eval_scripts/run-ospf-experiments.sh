d0bb83c958dc2691a07451b7faf39e28b402ffb5
Ahmed El-Hassany <eahemd@ethz.ch>
2017-09-20 03:09:05 +0200

    increase num processes and reqs in running

--- eval_scripts/run-ospf-experiments.sh
+++ eval_scripts/run-ospf-experiments.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # Generate evaluation values for all given reqs
-NUM_PROCESSES=1
-NUM_REPEATS=1
+NUM_PROCESSES=20
+NUM_REPEATS=5
 
 
 PATH_TO_TOPOS="topos/*/"
-#topos/small/Arnes topos/small/Bics.graphml topos/small/Canerie topos/small/Renater2008 topos/small/CrlNetworkServices;
-#topos/mid/Columbus.graphml topos/mid/Esnet.graphml topos/mid/Latnet.graphml topos/mid/Sinet.graphml topos/mid/Uninett2011.graphml
-#topos/large/Cogentco.graphml  topos/large/Colt.graphml	topos/large/GtsCe.graphml  topos/large/TataNld.graphml	topos/large/UsCarrier.graphml
+#topos/small/Arnes topos/small/Bics topos/small/Canerie topos/small/Renater2008 topos/small/CrlNetworkServices;
+#topos/mid/Columbus topos/mid/Esnet topos/mid/Latnet topos/mid/Sinet topos/mid/Uninett2011
+#topos/large/Cogentco topos/large/Colt	topos/large/GtsCe  topos/large/TataNld topos/large/UsCarrier
 
-for file in topos/small/Arnes topos/small/Canerie topos/small/Renater2008 topos/small/CrlNetworkServices;
+for file in topos/mid/Columbus topos/mid/Esnet topos/mid/Latnet topos/mid/Sinet topos/mid/Uninett2011;
 do
     topo="${file}.graphml"
     values="${file}_ospf_reqs.py "
     for syn in cegis concrete;
     do
-        #for reqs in 1 2 4 8 16;
-        for reqs in 1;
+        for reqs in 1 2 4 8 16;
+        #for reqs in 1;
         do
             for req_type in simple kconnected ecmp order;
             do
